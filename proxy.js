629c11b5405da2e3798f29790462d35031a1bc6f
Tim Heckel <tim@slatebox.com>
2014-06-07 22:29:46 -0500

    Started using the higher level RemoteCollectionDriver instead of MongoCollection to take advantage of allow/deny rules in addition

--- proxy.js
+++ proxy.js
@@ -13,13 +13,12 @@ MeteorDBProxy = function(opts) {
 			if (opts.oplogUrl) {
 				_.extend(_args, { oplogUrl: opts.oplogUrl });
 			}
-
 			//using RemoteCollectionDriver returns a _driver that can be used to hook
 			//into the Meteor.Collection api and then do all the mongo stuff (insert, upsert, etc)
 			//as well as leverage allow/deny rules on the collection itself
 			_opts = { _driver: new MongoInternals.RemoteCollectionDriver(opts.mongoUrl, _args) }
 		}
-		
+
 		_.each(opts.collections, function(c) { 
 			var _name = c.name || c.db;
 			_self[_name] = new Meteor.Collection(c.db, _opts);
