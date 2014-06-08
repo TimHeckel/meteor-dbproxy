a57513d9adbcc3af4d7dbb7b390ab13901f3c19f
Tim Heckel <tim@slatebox.com>
2014-06-07 22:29:09 -0500

    Started using the higher level  instead of  to take advantage of allow/deny rules in addition

--- proxy.js
+++ proxy.js
@@ -7,33 +7,23 @@ MeteorDBProxy = function(opts) {
 	//[{ db: "dbCollectionName", name: "MeteorCollectionName" }]
 
 	function _init() {
+		var _opts = {};
 		if (Meteor.isServer) {
 			var _args = {};
 			if (opts.oplogUrl) {
 				_.extend(_args, { oplogUrl: opts.oplogUrl });
 			}
-			var _db = new MongoInternals.Connection(opts.mongoUrl, _args);
-			_.each(opts.collections, function(c) {
 
-				_self[c.name] = {};
-				var _ret = {};
-				_.each(opts.bindables, function(m) {
-					_ret[m] = _.bind(_db[m], _db, c.db);
-				});
-
-				//use name if passed in, otherwise default to the db collection name
-				var _name = c.name || c.db;
-
-				_.extend(_self[_name], _ret);
-			});
+			//using RemoteCollectionDriver returns a _driver that can be used to hook
+			//into the Meteor.Collection api and then do all the mongo stuff (insert, upsert, etc)
+			//as well as leverage allow/deny rules on the collection itself
+			_opts = { _driver: new MongoInternals.RemoteCollectionDriver(opts.mongoUrl, _args) }
 		}
-
-		if (Meteor.isClient) {
-			_.each(opts.collections, function(c) { 
-				var _name = c.name || c.db;
-				_self[_name] = new Meteor.Collection(c.db);
-			});
-		}
+		
+		_.each(opts.collections, function(c) { 
+			var _name = c.name || c.db;
+			_self[_name] = new Meteor.Collection(c.db, _opts);
+		});
 	};
 
 	_init();
